fff41e5a9bc3c684da1b43786952b9c50e4a0b35
David Chelimsky <david@chelimac.local>
2008-07-18 21:33:35 -0500

    alphabetize the generated files

--- generators/rspec/rspec_generator.rb
+++ generators/rspec/rspec_generator.rb
@@ -14,14 +14,16 @@ class RspecGenerator < Rails::Generator::Base
     record do |m|
       script_options     = { :chmod => 0755, :shebang => options[:shebang] == DEFAULT_SHEBANG ? nil : options[:shebang] }
 
-      m.directory 'spec'
-      m.template  'spec_helper.rb',                'spec/spec_helper.rb'
-      m.file      'spec.opts',                     'spec/spec.opts'
-      m.file      'rcov.opts',                     'spec/rcov.opts'
-      m.file      'script/spec_server',            'script/spec_server', script_options
-      m.file      'script/spec',                   'script/spec',        script_options
-      m.file      'script/autospec',               'script/autospec',    script_options
       m.file      'rspec.rake',                    'lib/tasks/rspec.rake'
+
+      m.file      'script/autospec',               'script/autospec',    script_options
+      m.file      'script/spec',                   'script/spec',        script_options
+      m.file      'script/spec_server',            'script/spec_server', script_options
+
+      m.directory 'spec'
+      m.file      'rcov.opts',                     'spec/rcov.opts'
+      m.file      'spec.opts',                     'spec/spec.opts'
+      m.template  'spec_helper.rb',                'spec/spec_helper.rb'
 
       m.directory 'stories'
       m.file      'all_stories.rb',                'stories/all.rb'
