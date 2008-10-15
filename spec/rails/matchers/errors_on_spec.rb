5fcd7d127a7e2dcb00323cf4dacc3d53b7d23e52
Mike Vincent <mike@cryingwhilecoding.com>
2008-10-15 08:28:54 -0500

    Fix issue with failure message not correctly displaying the actual count for error(s)_on failures [#566 state:resolved]

--- spec/rails/matchers/errors_on_spec.rb
+++ spec/rails/matchers/errors_on_spec.rb
@@ -11,3 +11,20 @@ describe "errors_on" do
     have(2).errors_on(:whatever).description.should == "should have 2 errors on :whatever"
   end
 end
+
+describe "error_on, failure message" do
+  it "should provide a message including the number actually given" do
+    lambda {
+      [].should have(1).error_on(:whatever)
+    }.should fail_with("expected 1 error on :whatever, got 0")
+  end
+end
+
+describe "errors_on, failure message" do
+  it "should provide a message including the number actually given" do
+    lambda {
+      [1].should have(3).errors_on(:whatever)
+    }.should fail_with("expected 3 errors on :whatever, got 1")
+  end
+end
+
