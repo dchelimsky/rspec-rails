bd52e97d87e729d26904aab565e94de8de0e78ab
David Chelimsky <dchelimsky@gmail.com>
2009-01-11 09:36:32 -0600

    Ensure that errors captured by rescue_from are not allowed to bubble up to code examples. [#85]

--- spec/spec/rails/example/controller_spec_spec.rb
+++ spec/spec/rails/example/controller_spec_spec.rb
@@ -161,23 +161,51 @@ require 'controller_spec_controller'
     end
     
     describe "with an error that is not rescued in the controller" do
-      it "should raise the error" do
-        lambda do
-          get 'other_error_action'
-        end.should raise_error(ControllerSpecController::OtherError)
+      context "without rails' error handling" do
+        it "raises the error" do
+          lambda do
+            get 'other_error_action'
+          end.should raise_error(ControllerSpecController::UnRescuedError)
+        end
+      end
+      context "with rails' error handling" do
+        it "does not raise the error" do
+          controller.use_rails_error_handling!
+          lambda do
+            get 'other_error_action'
+          end.should_not raise_error
+        end
       end
     end
     
     describe "with an error that is rescued in the controller" do
-      it "should not raise error" do
-        lambda do
+      context "without rails' error handling" do
+        it "does not raise error" do
+          lambda do
+            get 'rescued_error_action'
+          end.should_not raise_error
+        end
+
+        it "executes rescue_from" do
           get 'rescued_error_action'
-        end.should_not raise_error
+          response.body.should == 'Rescued!'
+        end
       end
 
-      it "should execute rescue_from" do
-        get 'rescued_error_action'
-        response.body.should == 'Rescued!'
+      context "with rails' error handling" do
+        before(:each) do
+          controller.use_rails_error_handling!
+        end
+        it "does not raise error" do
+          lambda do
+            get 'rescued_error_action'
+          end.should_not raise_error
+        end
+
+        it "executes rescue_from" do
+          get 'rescued_error_action'
+          response.body.should == 'Rescued!'
+        end
       end
     end
 
