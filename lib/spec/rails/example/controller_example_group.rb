bdcf8d21c10a56af98113765f509daf063bfaaa5
David Chelimsky <dchelimsky@gmail.com>
2009-02-12 00:46:49 -0600

    move routing helpers out to its own module

--- lib/spec/rails/example/controller_example_group.rb
+++ lib/spec/rails/example/controller_example_group.rb
@@ -84,6 +84,8 @@ module Spec
           end
         end
         
+        include ::Spec::Rails::Example::RoutingHelpers
+        
         before(:each) do
           # Some Rails apps explicitly disable ActionMailer in environment.rb
           if defined?(ActionMailer)
@@ -116,54 +118,6 @@ module Spec
           @integrate_views = self.class.integrate_views?
         end
         
-        class RouteForMatcher
-          def initialize(example, options)
-            @example, @options = example, options
-          end
-          
-          def ==(expected)
-            if Hash === expected
-              path, querystring = expected[:path].split('?')
-              path = expected.merge(:path => path)
-            else
-              path, querystring = expected.split('?')
-            end
-            params = querystring.blank? ? {} : @example.params_from_querystring(querystring)
-            @example.assert_recognizes(@options, path, params)
-            true
-          end
-        end
-
-        # Uses ActionController::Routing::Routes to generate
-        # the correct route for a given set of options.
-        # == Example
-        #   route_for(:controller => 'registrations', :action => 'edit', :id => 1)
-        #     => '/registrations/1;edit'
-        def route_for(options)
-          RouteForMatcher.new(self, options)
-        end
-
-        # Uses ActionController::Routing::Routes to parse
-        # an incoming path so the parameters it generates can be checked
-        # == Example
-        #   params_from(:get, '/registrations/1/edit')
-        #     => :controller => 'registrations', :action => 'edit', :id => 1
-        def params_from(method, path)
-          ensure_that_routes_are_loaded
-          path, querystring = path.split('?')
-          params = ActionController::Routing::Routes.recognize_path(path, :method => method)
-          querystring.blank? ? params : params.merge(params_from_querystring(querystring))
-        end
-
-        def params_from_querystring(querystring) # :nodoc:
-          params = {}
-          querystring.split('&').each do |piece|
-            key, value = piece.split('=')
-            params[key.to_sym] = value
-          end
-          params
-        end
-
       protected
 
         def _assigns_hash_proxy
---
+++ lib/spec/rails/example/routing_helpers.rb
@@ -0,0 +1,57 @@
+module Spec
+  module Rails
+    module Example
+      module RoutingHelpers
+        
+        class RouteFor
+          def initialize(example, options)
+            @example, @options = example, options
+          end
+  
+          def ==(expected)
+            if Hash === expected
+              path, querystring = expected[:path].split('?')
+              path = expected.merge(:path => path)
+            else
+              path, querystring = expected.split('?')
+            end
+            params = querystring.blank? ? {} : @example.params_from_querystring(querystring)
+            @example.assert_recognizes(@options, path, params)
+            true
+          end
+        end
+
+        # Uses ActionController::Routing::Routes to generate
+        # the correct route for a given set of options.
+        # == Example
+        #   route_for(:controller => 'registrations', :action => 'edit', :id => 1)
+        #     => '/registrations/1;edit'
+        def route_for(options)
+          RouteFor.new(self, options)
+        end
+
+        # Uses ActionController::Routing::Routes to parse
+        # an incoming path so the parameters it generates can be checked
+        # == Example
+        #   params_from(:get, '/registrations/1/edit')
+        #     => :controller => 'registrations', :action => 'edit', :id => 1
+        def params_from(method, path)
+          ensure_that_routes_are_loaded
+          path, querystring = path.split('?')
+          params = ActionController::Routing::Routes.recognize_path(path, :method => method)
+          querystring.blank? ? params : params.merge(params_from_querystring(querystring))
+        end
+
+        def params_from_querystring(querystring) # :nodoc:
+          params = {}
+          querystring.split('&').each do |piece|
+            key, value = piece.split('=')
+            params[key.to_sym] = value
+          end
+          params
+        end
+
+      end
+    end
+  end
+end
