259672c2fcb3e57de0a96a757ab002063bf92644
David Chelimsky <dchelimsky@gmail.com>
2009-02-15 17:43:04 -0600

    words

--- lib/spec/rails/extensions/action_controller/test_response.rb
+++ lib/spec/rails/extensions/action_controller/test_response.rb
@@ -9,9 +9,10 @@ module ActionController #:nodoc:
     if ::Rails::VERSION::STRING < "2.3"
       def [](name)
         Kernel.warn <<-WARNING
-[](name) as an alias for capture(name) (TestResponse extension in rspec-rails)
-is deprecated and will be removed in the rspec-rails release that follows the
-rails-2.3.0 release.
+DEPRECATION NOTICE: [](name) as an alias for capture(name) (TestResponse
+extension in rspec-rails) is deprecated and will not be defined by rspec-rails
+when working with rails >= 2.3.0. It will also be removed entirely from
+a future version of rspec-rails.
 WARNING
         capture(name)
       end
