8b9b8f1c1ed20d8725a35ed1969d2f022971d17b
David Chelimsky <dchelimsky@gmail.com>
2009-02-15 15:31:36 -0600

    OK - REALLY Got use_rails_error_handling working correctly for all supported versions of rails.
    - 2.0.5,2.1.2,2.2.2,2.3.0,current edge
    - by default, all un-rescued errors are raised
    - with controller.use_rails_error_handling!, errors
      are not raised, but are reflected in response.response_code
    - rescued errors are not re-raised by use_rails_error_handling!
    - we'll be adding a new mechanism to bypass rescue_from in
      order to specify errors from an action directly

--- lib/spec/rails/extensions/action_controller/rescue.rb
+++ lib/spec/rails/extensions/action_controller/rescue.rb
@@ -1,30 +1,26 @@
 module ActionController
   module Rescue
     def use_rails_error_handling!
-      @use_rails_error_handling = true
+      if Rails::VERSION::STRING =~ /^2\.0/
+        @use_rails_error_handling = true
+      else
+        # anything but 0.0.0.0 - borrowed from rails own rescue_action_in_public!
+        request.remote_addr = '208.77.188.166'
+      end
     end
-
+    
     def use_rails_error_handling?
       @use_rails_error_handling ||= false
     end
 
   protected
   
-    if Rails::VERSION::STRING =~ /^2\.(2|3)/
-      def rescue_action_with_fast_errors(exception)
-        if use_rails_error_handling?
-          rescue_action_without_fast_errors exception
-        else
-          raise exception
-        end
-      end
-      alias_method_chain :rescue_action, :fast_errors
-    else
+    if Rails::VERSION::STRING =~ /^2\.0/
       def rescue_action_with_handler_with_fast_errors(exception)
-        if use_rails_error_handling?
-          rescue_action_with_handler_without_fast_errors exception
+        if use_rails_error_handling? & !handler_for_rescue(exception)
+          rescue_action_in_public(exception)
         else
-          raise exception
+          rescue_action_with_handler_without_fast_errors(exception)
         end
       end
       alias_method_chain :rescue_action_with_handler, :fast_errors
--- spec/spec/rails/example/error_handling_spec.rb
+++ spec/spec/rails/example/error_handling_spec.rb
@@ -5,53 +5,38 @@ require 'controller_spec_controller'
   describe "A controller example running in #{mode} mode", :type => :controller do
     controller_name :controller_spec
     integrate_views if mode == 'integration'
-
-    describe "with an error that is not rescued in the controller" do
-      context "without rails' error handling" do
-        it "raises the error" do
-          lambda do
-            get 'un_rescued_error_action'
-          end.should raise_error(ControllerSpecController::UnRescuedError)
-        end
-      end
-
-      context "with rails' error handling" do
-        it "raises the error" do
-          controller.use_rails_error_handling!
-          lambda do
-            get 'un_rescued_error_action'
-          end.should raise_error(ControllerSpecController::UnRescuedError)
-        end
-      end
-    end
     
-    describe "with an error that is rescued in the controller" do
-      context "without rails' error handling" do
+    describe "without use_rails_error_handling!" do
+      describe "with an error that is *not* rescued" do
         it "raises the error" do
           lambda do
-            get 'rescued_error_action'
-          end.should raise_error(ControllerSpecController::RescuedError)
+            get 'un_rescued_error_action'
+          end.should raise_error(ControllerSpecController::UnRescuedError)
         end
       end
-
-      context "with rails' error handling" do
-        before(:each) do
-          controller.use_rails_error_handling!
-        end
-
-        it "does not raise error" do
-          lambda do
-            get 'rescued_error_action'
-          end.should_not raise_error
-        end
-
-        it "executes rescue_from" do
+      describe "with an error that *is* rescued" do
+        it "returns a 200" do
           get 'rescued_error_action'
-          response.body.should == 'Rescued!'
+          response.response_code.should == 200
         end
       end
     end
 
-
+    describe "with use_rails_error_handling!" do
+      describe "with an error that is *not* rescued" do
+        it "returns the error code" do
+          controller.use_rails_error_handling!
+          get 'un_rescued_error_action'
+          response.response_code.should == 500
+        end
+      end
+      describe "with an error that *is* rescued" do
+        it "returns a 200" do
+          controller.use_rails_error_handling!
+          get 'rescued_error_action'
+          response.response_code.should == 200
+        end
+      end
+    end
   end
 end
