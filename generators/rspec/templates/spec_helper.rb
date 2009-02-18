81531c881472c4f60cd8319af4ff3c3122876431
David Chelimsky <dchelimsky@gmail.com>
2009-02-18 01:03:52 -0600

    add spec/autorun to spec_helper.rb (to run individual specs w/ ruby

--- generators/rspec/templates/spec_helper.rb
+++ generators/rspec/templates/spec_helper.rb
@@ -2,7 +2,7 @@
 # from the project root directory.
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'spec'
+require 'spec/autorun'
 require 'spec/rails'
 
 Spec::Runner.configure do |config|
