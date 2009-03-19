7d4d76f5ff4cfdeaf4ee8a40310916a54ba0d38b
Matt Patterson <matt@reprocessed.org>
2009-03-19 17:07:38 +0000

    Fix for Lightouse ticket #739
    [#739 state:resolved milestone:'Next Release']

--- spec/autotest/mappings_spec.rb
+++ spec/autotest/mappings_spec.rb
@@ -33,4 +33,54 @@ describe Autotest::RailsRspec, "file mapping" do
                             to('app/views/things/index.html.erb')
   end
   
+  describe "between routes.rb and things which depend on routes" do
+    it "should map routes.rb to controllers" do
+      @autotest.should map_specs(['spec/controllers/things_controller_spec.rb']).
+                              to('config/routes.rb')
+    end
+    
+    it "should map routes.rb to views" do
+      @autotest.should map_specs(['spec/views/things/action.html.erb_spec.rb']).
+                              to('config/routes.rb')
+    end
+    
+    it "should map routes.rb to helpers" do
+      @autotest.should map_specs(['spec/helpers/things_helper_spec.rb']).
+                              to('config/routes.rb')
+    end
+    
+    it "should map routing example to routes" do
+      @autotest.should map_specs(['spec/routing/thing_spec.rb']).
+                              to('config/routes.rb')
+    end
+  end  
+  
+  describe "between the config and specs" do
+    ['spec/controllers/things_controller_spec.rb', 
+     'spec/views/things/action.html.erb_spec.rb', 
+     'spec/helpers/things_helper_spec.rb', 
+     'spec/routing/thing_spec.rb', 
+     'spec/models/thing_spec.rb'].each do |file_path|
+    
+      it "should map environment.rb to #{file_path}" do
+        @autotest.should map_specs([file_path]).
+                                to('config/environment.rb')
+      end
+      
+      it "should map environments/test.rb to #{file_path}" do
+        @autotest.should map_specs([file_path]).
+                                to('config/environments/test.rb')
+      end
+      
+      it "should map boot.rb to #{file_path}" do
+        @autotest.should map_specs([file_path]).
+                                to('config/boot.rb')
+      end
+      
+      it "should map spec_helper.rb to #{file_path}" do
+        @autotest.should map_specs([file_path]).
+                                to('spec/spec_helper.rb')
+      end
+    end
+  end
 end
