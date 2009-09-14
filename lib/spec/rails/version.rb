70041db8031b3d4a6fb31198105c23473fcc1c99
David Chelimsky <dchelimsky@gmail.com>
2009-09-14 18:54:52 -0500

    bump version to 1.2.9.rc1

--- lib/spec/rails/version.rb
+++ lib/spec/rails/version.rb
@@ -2,12 +2,12 @@ module Spec # :nodoc:
   module Rails # :nodoc:
     module VERSION # :nodoc:
       unless defined? MAJOR
-        MAJOR       = 1
-        MINOR       = 2
-        TINY        = 9
-        REALLY_TINY = nil
+        MAJOR  = 1
+        MINOR  = 2
+        TINY   = 9
+        PRE    = 'rc1'
       
-        STRING = [MAJOR, MINOR, TINY, REALLY_TINY].compact.join('.')
+        STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
 
         SUMMARY = "rspec-rails #{STRING}"
       end
