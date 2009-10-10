3e01367dfd5b743a4cb1fc4475fe471c0594ac06
David Chelimsky <dchelimsky@gmail.com>
2009-10-10 04:21:20 -0500

    use stub() in generated scaffold

--- generators/rspec_scaffold/templates/controller_spec.rb
+++ generators/rspec_scaffold/templates/controller_spec.rb
@@ -8,7 +8,7 @@ describe <%= controller_class_name %>Controller do
 
   describe "GET index" do
     it "assigns all <%= table_name.pluralize %> as @<%= table_name.pluralize %>" do
-      <%= class_name %>.stub!(:find).with(:all).and_return([mock_<%= file_name %>])
+      <%= class_name %>.stub(:find).with(:all).and_return([mock_<%= file_name %>])
       get :index
       assigns[:<%= table_name %>].should == [mock_<%= file_name %>]
     end
@@ -16,7 +16,7 @@ describe <%= controller_class_name %>Controller do
 
   describe "GET show" do
     it "assigns the requested <%= file_name %> as @<%= file_name %>" do
-      <%= class_name %>.stub!(:find).with("37").and_return(mock_<%= file_name %>)
+      <%= class_name %>.stub(:find).with("37").and_return(mock_<%= file_name %>)
       get :show, :id => "37"
       assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
     end
@@ -24,7 +24,7 @@ describe <%= controller_class_name %>Controller do
 
   describe "GET new" do
     it "assigns a new <%= file_name %> as @<%= file_name %>" do
-      <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>)
+      <%= class_name %>.stub(:new).and_return(mock_<%= file_name %>)
       get :new
       assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
     end
@@ -32,7 +32,7 @@ describe <%= controller_class_name %>Controller do
 
   describe "GET edit" do
     it "assigns the requested <%= file_name %> as @<%= file_name %>" do
-      <%= class_name %>.stub!(:find).with("37").and_return(mock_<%= file_name %>)
+      <%= class_name %>.stub(:find).with("37").and_return(mock_<%= file_name %>)
       get :edit, :id => "37"
       assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
     end
@@ -42,13 +42,13 @@ describe <%= controller_class_name %>Controller do
 
     describe "with valid params" do
       it "assigns a newly created <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.stub!(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => true))
+        <%= class_name %>.stub(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => true))
         post :create, :<%= file_name %> => {:these => 'params'}
         assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
       end
 
       it "redirects to the created <%= file_name %>" do
-        <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => true))
+        <%= class_name %>.stub(:new).and_return(mock_<%= file_name %>(:save => true))
         post :create, :<%= file_name %> => {}
         response.should redirect_to(<%= table_name.singularize %>_url(mock_<%= file_name %>))
       end
@@ -56,13 +56,13 @@ describe <%= controller_class_name %>Controller do
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.stub!(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => false))
+        <%= class_name %>.stub(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => false))
         post :create, :<%= file_name %> => {:these => 'params'}
         assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
       end
 
       it "re-renders the 'new' template" do
-        <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => false))
+        <%= class_name %>.stub(:new).and_return(mock_<%= file_name %>(:save => false))
         post :create, :<%= file_name %> => {}
         response.should render_template('new')
       end
@@ -80,13 +80,13 @@ describe <%= controller_class_name %>Controller do
       end
 
       it "assigns the requested <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
+        <%= class_name %>.stub(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
         put :update, :id => "1"
         assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
       end
 
       it "redirects to the <%= file_name %>" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
+        <%= class_name %>.stub(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
         put :update, :id => "1"
         response.should redirect_to(<%= table_name.singularize %>_url(mock_<%= file_name %>))
       end
@@ -100,13 +100,13 @@ describe <%= controller_class_name %>Controller do
       end
 
       it "assigns the <%= file_name %> as @<%= file_name %>" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
+        <%= class_name %>.stub(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
         put :update, :id => "1"
         assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
       end
 
       it "re-renders the 'edit' template" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
+        <%= class_name %>.stub(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
         put :update, :id => "1"
         response.should render_template('edit')
       end
@@ -122,7 +122,7 @@ describe <%= controller_class_name %>Controller do
     end
 
     it "redirects to the <%= table_name %> list" do
-      <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:destroy => true))
+      <%= class_name %>.stub(:find).and_return(mock_<%= file_name %>(:destroy => true))
       delete :destroy, :id => "1"
       response.should redirect_to(<%= table_name %>_url)
     end
