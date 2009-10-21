211b779c3efbd134a4cb7690243bc1f68c93eafa
Tom Stuart <tom@experthuman.com>
2009-10-21 10:10:34 +0100

    Remove renegade apostrophe from possessive its in spec_helper.rb template
    [#901 state:resolved milestone:'Next Release']

--- generators/rspec/templates/spec_helper.rb
+++ generators/rspec/templates/spec_helper.rb
@@ -41,7 +41,7 @@ Spec::Runner.configure do |config|
   #
   # == Mock Framework
   #
-  # RSpec uses it's own mocking framework by default. If you prefer to
+  # RSpec uses its own mocking framework by default. If you prefer to
   # use mocha, flexmock or RR, uncomment the appropriate line:
   #
   # config.mock_with :mocha
