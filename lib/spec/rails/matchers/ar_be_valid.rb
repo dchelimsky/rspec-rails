559fe6e45c48696717c0d7562ccf1aae31190d1a
David Chelimsky <dchelimsky@gmail.com>
2010-01-11 20:49:05 -0600

    be_valid matcher for AR objects uses matcher DSL

--- lib/spec/rails/matchers/ar_be_valid.rb
+++ lib/spec/rails/matchers/ar_be_valid.rb
@@ -1,46 +1,23 @@
 if defined?(ActiveRecord::Base)
-  module Spec
-    module Rails
-      module Matchers
-    
-        class ArBeValid  #:nodoc:
-        
-          def initialize
-            @matcher = Spec::Matchers::Be.new :be_valid
-            @matcher.__send__ :handling_predicate!
-          end
-
-          def matches?(actual)
-            @actual = actual
-            @matcher.matches? @actual
-          end
-      
-          def failure_message_for_should
-            if @actual.respond_to?(:errors) &&
-                ActiveRecord::Errors === @actual.errors
-              "Expected #{@actual.inspect} to be valid, but it was not\nErrors: " + @actual.errors.full_messages.join(", ")            
-            else
-              @matcher.failure_message_for_should
-            end
-          end
-        
-          def failure_message_for_should_not
-            @matcher.failure_message_for_should_not
-          end
-        
-          def description
-            "be valid"
-          end
+  module Spec::Rails::Matchers
+    # :call-seq:
+    #   response.should be_valid
+    #   response.should_not be_valid
+    def be_valid
+      ::Spec::Matchers::Matcher.new :be_valid do
+        match do |actual|
+          actual.valid?
         end
 
-        # :call-seq:
-        #   response.should be_valid
-        #   response.should_not be_valid
-        def be_valid
-          ArBeValid.new
+        failure_message_for_should do |actual|
+          if actual.respond_to?(:errors) && ActiveRecord::Errors === actual.errors
+            "Expected #{actual.inspect} to be valid, but it was not\nErrors: " + actual.errors.full_messages.join(", ")            
+          else
+            "Expected #{actual.inspect} to be valid"
+          end
         end
-    
       end
     end
+
   end
 end
