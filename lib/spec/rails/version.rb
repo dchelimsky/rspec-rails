919e6a723ab967c09ce7d9c10f55cbdf65c66f74
David Chelimsky <dchelimsky@gmail.com>
2010-01-13 08:16:20 -0600

    prep for 1.3.2

--- lib/spec/rails/version.rb
+++ lib/spec/rails/version.rb
@@ -4,7 +4,7 @@ module Spec # :nodoc:
       unless defined? MAJOR
         MAJOR  = 1
         MINOR  = 3
-        TINY   = 1
+        TINY   = 2
         PRE    = nil
       
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
